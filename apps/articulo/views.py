ca231e940664bffbe988168a79c9b622ec69a3b3
pablopalillo <pablo.576@hotmail.com>
2017-05-30 22:46:25 -0500

    get article x multimedias

--- apps/articulo/views.py
+++ apps/articulo/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from .models import Articulo
 from .models import Meta
+from apps.multimedia.models import Articuloxmultimedia
 
 def getArticle(request, slug):
 
@@ -17,7 +18,8 @@ def getArticle(request, slug):
             if article.estado.id_estado != 1 :
                 raise Http404("Error 404, Articulo no encontrado")
             else:
-                metaArticle = getMetaData(article.id_articulo)
+                metaArticle     = getMetaData(article.id_articulo)
+                imagesArticle   = getPostImages(article.id_articulo)
 
                 return render(request, "articulo/index.html",
                     {"article":article, 'meta':metaArticle}
@@ -42,6 +44,12 @@ def getMetaData(idPost):
             metaHtml = metaHtml+" "+metaText
 
     return metaHtml
+
+def getPostImages(idPost):
+
+    if( not idPost is None):
+        postImages = Articuloxmultimedia.objects.filter(articulo = idPost)
+        print(postImages)
 
 
 
